3709fcb8545ba0e01687333aa764ab03438016d6
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2020-08-28 18:14:20 +0300

    HW-2

--- HW-1.py
+++ HW-1.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from group import Group
 
 
 class test(unittest.TestCase):
@@ -11,22 +12,22 @@ class test(unittest.TestCase):
     def test_create_group(self):
         driver = self.driver
         self.open_home_page(driver)
-        self.login(driver)
+        self.login(driver, username="admin", password="secret")
         self.open_group_list(driver)
         self.group_creation(driver)
-        self.fill_group_form(driver)
+        self.fill_group_form(driver, Group(name="1st group", header="logo", footer="comment"))
         self.submit_group_creation(driver)
 
     def submit_group_creation(self, driver):
         driver.find_element_by_name("submit").click()
 
-    def fill_group_form(self, driver):
+    def fill_group_form(self, driver, group):
         driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").send_keys("1st group")
+        driver.find_element_by_name("group_name").send_keys(group.name)
         driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").send_keys("logo")
+        driver.find_element_by_name("group_header").send_keys(group.header)
         driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").send_keys("comment")
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
 
     def group_creation(self, driver):
         driver.find_element_by_name("new").click()
@@ -34,11 +35,11 @@ class test(unittest.TestCase):
     def open_group_list(self, driver):
         driver.find_element_by_link_text("groups").click()
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_id("LoginForm").click()
         driver.find_element_by_xpath("//input[@value='Login']").click()
 
