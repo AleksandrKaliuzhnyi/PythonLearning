065797dbb391f84389634817ec02add45a9c9139
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2020-08-28 17:17:01 +0300

    HW-2

--- HW-1.py
+++ HW-1.py
@@ -1,65 +1,52 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 
 class test(unittest.TestCase):
+
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
 
-    def test_app_dynamics_job(self):
+    def test_create_group(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook/")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_id("LoginForm").click()
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_name("new").click()
+        self.open_home_page(driver)
+        self.login(driver)
+        self.open_group_list(driver)
+        self.group_creation(driver)
+        self.fill_group_form(driver)
+        self.submit_group_creation(driver)
+
+    def submit_group_creation(self, driver):
+        driver.find_element_by_name("submit").click()
+
+    def fill_group_form(self, driver):
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").send_keys("1st group")
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").send_keys("logo")
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").send_keys("comment")
-        driver.find_element_by_name("submit").click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
+    def group_creation(self, driver):
+        driver.find_element_by_name("new").click()
 
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to.alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
+    def open_group_list(self, driver):
+        driver.find_element_by_link_text("groups").click()
 
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to.alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
+    def login(self, driver):
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_id("LoginForm").click()
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook/")
 
     def tearDown(self):
-         self.assertEqual([], self.verificationErrors)
-
+         self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
