95180b3fead296e2cc35622d80e693f1eb12d949
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2020-08-29 14:07:10 +0300

    HW-3

---
+++ test/create_contact.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import unittest
+from contact import Contact
+
+
+
+class test2(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def test_create_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.create_contact(wd, Contact("Oleksandr", "Kaliuzhnyi", "emkiss", "Infopulse", "Kiev", "0931234567", "QA",
+                            "test@gmail.com", "7", "December", "1994"))
+        self.back_to_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def back_to_home_page(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def create_contact(self, wd, contact):
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_num)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").send_keys(contact.work)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
