22de06e9f8d28bc3d4eeeddb09498b413faf49e2
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2020-09-01 13:15:13 +0300

    HW-4

---
+++ fixture/application_group.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+
+
+class Application_group:
+
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def submit_group_creation(self):
+        wd = self.wd
+        wd.find_element_by_name("submit").click()
+
+    def group_creation(self, group):
+        wd = self.wd
+        self.open_group_list()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.submit_group_creation()
+
+    def open_group_list(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def destroy(self):
+        self.wd.quit()
--- test/create_group.py
+++ test/create_group.py
@@ -1,53 +1,14 @@
-from selenium import webdriver
-import unittest
+import pytest
 from group import Group
+from application_group import Application_group
 
+@pytest.fixture
+def app(request):
+    fixture = Application_group()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class test(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def test_create_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_group_list(wd)
-        self.group_creation(wd, Group(name="1st group", header="logo", footer="comment"))
-        self.submit_group_creation(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def submit_group_creation(self, wd):
-        wd.find_element_by_name("submit").click()
-
-    def group_creation(self, wd, group):
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-
-    def open_group_list(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-         self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_create_group(app):
+    app.login( username="admin", password="secret")
+    app.group_creation( Group(name="1st group", header="logo", footer="comment"))
+    app.logout()
